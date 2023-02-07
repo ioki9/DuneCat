eb347626d90e2eae0bee180cc398d95ce7365415
ioki9 <ioki9@hotmail.com>
2023-02-08 00:20:35 +0300

    make.yml tools.h update

--- src/DuneCat.App/tools.h
+++ src/DuneCat.App/tools.h
@@ -7,7 +7,7 @@ namespace tools
     #ifdef Q_OS_WIN
     bool bootUpStart(bool isOn);
     #elif defined(Q_OS_UNIX)
-    bool bootUpStart(bool isOn)
+    bool bootUpStart(bool isOn);
     #endif
     QString macOSXAppBundlePath();
     QString macOSXAppBundleName();
