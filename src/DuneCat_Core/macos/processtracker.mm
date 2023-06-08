54c865d81761acb1ff7e12049f45806fe3381318
ioki9 <ioki9@hotmail.com>
2023-06-09 02:21:36 +0300

    Refacotr: mac fix

--- src/DuneCat_Core/macos/processtracker.mm
+++ src/DuneCat_Core/macos/processtracker.mm
@@ -5,17 +5,16 @@
 #import "OSXProcessObserver.h"
 #import <Cocoa/Cocoa.h>
 
-namespace DuneCat
-{
-std::vector<DCProcessInfo> get_bsd_process_list()
+
+std::vector<ProcessInfo> get_bsd_process_list()
 {
     pid_t pids[2048];
-    std::vector<DCProcessInfo> result{};
+    std::vector<ProcessInfo> result{};
     int bytes = proc_listpids(PROC_ALL_PIDS,0,pids,sizeof(pids));
     int n_proc = bytes/sizeof(pids[0]);
     for(int i{0};i<n_proc;i++)
     {
-        DCProcessInfo proc_info;
+        ProcessInfo proc_info;
         struct proc_bsdinfo proc;
         int st = proc_pidinfo(pids[i],PROC_PIDTBSDINFO,0,&proc,PROC_PIDTBSDINFO_SIZE);
         if(st == PROC_PIDTBSDINFO_SIZE)
@@ -34,13 +33,13 @@ std::vector<DCProcessInfo> get_bsd_process_list()
     return result;
 }
 
-std::vector<DCProcessInfo> get_gui_process_list()
+std::vector<ProcessInfo> get_gui_process_list()
 {
-    std::vector<DCProcessInfo> result;
+    std::vector<ProcessInfo> result;
     for (NSRunningApplication *app in
            [[NSWorkspace sharedWorkspace] runningApplications])
       {
-        DCProcessInfo proc;
+        ProcessInfo proc;
         proc.name = QString::fromNSString([app localizedName]);
         proc.pid = [app processIdentifier];
         proc.file_path =  QString::fromNSString([[app executableURL] absoluteString]);
@@ -50,34 +49,34 @@ std::vector<DCProcessInfo> get_gui_process_list()
 }
 
 OSXProcessObserver* observer;
-DCProcessTracker::DCProcessTracker(QObject* parent)
+ProcessTracker::ProcessTracker(QObject* parent)
 {
     observer = [[OSXProcessObserver alloc] init];
     [observer setCallback:this];
     [observer enableNotification];
 }
 
-DCProcessTracker::~DCProcessTracker()
+ProcessTracker::~ProcessTracker()
 {
     [observer release];
     observer = nil;
 }
 
-std::vector<DCProcessInfo> DCProcessTracker::get_process_list()
+std::vector<ProcessInfo> ProcessTracker::get_process_list()
 {
-    std::vector<DCProcessInfo> result = get_gui_process_list();
+    std::vector<ProcessInfo> result = get_gui_process_list();
     m_process_count = result.size();
     return result;
 }
 
-int DCProcessTracker::get_process_count()
+int ProcessTracker::get_process_count()
 {
     if(m_process_count != -1)
         return m_process_count;
     pid_t pids[2048];
-    std::vector<DCProcessInfo> result{};
+    std::vector<ProcessInfo> result{};
     int bytes = proc_listpids(PROC_ALL_PIDS,0,pids,sizeof(pids));
     m_process_count = bytes/sizeof(pids[0]);
     return m_process_count;
 }
-}
+
