dffd4b5421461eccb7033111ee54aa433db0b48d
ioki9 <ioki9@hotmail.com>
2023-06-15 22:53:58 +0300

    refactor: add [[maybe unused]]

--- src/DuneCat_Core/processinfo.h
+++ src/DuneCat_Core/processinfo.h
@@ -1,20 +1,18 @@
-#ifndef DCPROCESSSTRUCTS_H
-#define DCPROCESSSTRUCTS_H
+#pragma once
 #include <QtGlobal>
 #include <QtCore>
+#include <QPixmap>
 namespace DuneCat{
 struct ProcessInfo{
     QString name;
-    QString description;
+    [[maybe_unused]] QString description;
     QString file_path;
-    QString command_line;
-    QString owner_user;
-    QString owner_domain;
+    [[maybe_unused]] QString command_line;
+    [[maybe_unused]] QString owner_user;
+    [[maybe_unused]] QString owner_domain;
     quint32 pid;
-    QDateTime termination_date;
+    [[maybe_unused]] QDateTime termination_date;
     QDateTime creation_date;
-
+    [[maybe_unused]] QPixmap icon;
 };
 }
-
-#endif // DCPROCESSSTRUCTS_H
