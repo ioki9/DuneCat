c41d148bd425767b3da36df824b32930b80dbcaa
ioki9 <ioki9@hotmail.com>
2023-06-24 02:07:52 +0300

    add default constructor,getters and create_connection function

--- src/DuneCat_Core/dbmanager.cpp
+++ src/DuneCat_Core/dbmanager.cpp
@@ -1,29 +1,38 @@
 #include "dbmanager.h"
-#include <span>
+
 namespace DuneCat
 {
-DBManager::DBManager(const QString& connection_name,const QString& database_name) :
-    m_connection_name{connection_name}, m_db_name{database_name}
+DBManager::DBManager(const QString& connection_name,const QString& database_name)
+    : m_connection_name{connection_name}, m_db_name{database_name},
+    m_driver_name{QStringLiteral("QSQLITE")}
 {
     m_db = QSqlDatabase::database(connection_name);
     if(!m_db.isValid())
-        m_db = QSqlDatabase::addDatabase(QStringLiteral("QSQLITE"),connection_name);
+        m_db = QSqlDatabase::addDatabase(m_driver_name,connection_name);
     if(!m_db.isValid())
     {
         qDebug() << "Database isn't valid. Failed with error:"<<m_db.lastError().text();
         m_is_valid = false;
         return;
     }
+
     m_db.setDatabaseName(database_name);
     m_is_valid = true;
 }
 
+DBManager::DBManager()
+    : m_connection_name{QStringLiteral("")}, m_db_name{QStringLiteral("")},
+    m_driver_name{QStringLiteral("QSQLITE")}
+
+{
+
+}
 
 DBManager::DBManager(DBManager&& other)
     : m_connection_name{std::move(other.m_connection_name)},m_db_name{std::move(other.m_db_name)}
 {
-    qDebug()<<"invoke move op";
     m_db = QSqlDatabase::database(m_connection_name,other.m_db.isOpen());
+
     if(m_db.isValid())
     {
         m_is_valid = true;
@@ -31,20 +40,13 @@ DBManager::DBManager(DBManager&& other)
     }
     else
         qDebug() <<"Moved database isn't valid. Failed with error:"<< m_db.lastError().text();
-
-    if(other.m_db.isOpen())
-        other.m_db.close();
-
 }
 
 DBManager& DBManager::operator=(DBManager&& other)
 {
-    qDebug()<<"invoke = op";
     if(m_db.isOpen())
         m_db.close();
     m_db = QSqlDatabase::database(other.m_connection_name,other.m_db.isOpen());
-    if(other.m_db.isOpen())
-        other.m_db.close();
 
     if(m_db.isValid())
     {
@@ -70,6 +72,42 @@ bool DBManager::open_connection()
 void DBManager::close_connection()
 {
     m_db.close();
+}
+
+bool DBManager::create_connection(QStringView connection_name,bool open)
+{
+    if(m_db.isValid())
+    {
+        m_is_valid = true;
+        if(m_db.isOpen() == open)
+            return true;
+
+        if(open)
+            return open_connection();
+        else
+            m_db.close();
+
+        return true;
+    }
+
+    m_db = QSqlDatabase::database(QLatin1String(connection_name),open);
+    if(m_db.isValid())
+        return m_is_valid = true;;
+
+
+    m_db = QSqlDatabase::addDatabase(m_driver_name,connection_name);
+    if(m_db.isValid())
+    {
+        m_is_valid = true;
+        if(open)
+            return open_connection();
+        return true;
+    }
+    else
+    {
+        qDebug() << "Database isn't valid. Failed with error:"<<m_db.lastError().text();
+        return m_is_valid = false;
+    }
 }
 
 DBManager::~DBManager()
--- src/DuneCat_Core/dbmanager.h
+++ src/DuneCat_Core/dbmanager.h
@@ -9,20 +9,27 @@ class DBManager
 {
 public:
     DBManager(const QString& connection_name, const QString &database_name);
+    DBManager();
     ~DBManager();
     DBManager(DBManager&& other);
     DBManager& operator=(DBManager&& other);
     DBManager(const DBManager& copy)= delete;
     DBManager& operator=(const DBManager& copy) = delete;
+    bool create_connection(QStringView connection_name,bool open);
     bool open_connection();
     void close_connection();
+    inline void set_database_name(QStringView name){m_db_name = name; m_db.setDatabaseName(name);}
+    inline void set_driver_name(QStringView driver = QStringLiteral("QSQLITE")){m_driver_name = driver;}
     [[nodiscard]] inline bool is_valid() const {return m_is_valid;}
-    //TODO: add function to try adding a database again.
+    [[nodiscard]] inline QString get_database_name() const { return m_db_name;}
+    [[nodiscard]] inline QString get_connection_name() const {return m_connection_name;}
+
 private:
     QSqlDatabase m_db;
     bool m_is_valid{false};
     QString m_connection_name;
     QString m_db_name;
+    QString m_driver_name;
 };
 }
 #endif // DBMANAGER_H
