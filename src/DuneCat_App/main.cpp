574670ebd8d115810bc83e43689b9379d55d32e4
ioki9 <ioki9@hotmail.com>
2023-07-05 06:03:30 +0300

    Refactor(db): Commit first batch of processes using transaction.

--- src/DuneCat_App/main.cpp
+++ src/DuneCat_App/main.cpp
@@ -9,85 +9,9 @@
 
 using namespace DuneCat;
 
-//connects db to ProcessTracker signals
-bool init_connect_db()
-{
-    ProcessTracker* tracker = ProcessTracker::get_instance();
-    static DBManager db{"processes_conn","data.db"};
-    db.open();
-
-    static QSqlQuery query_create{db.get_database()};
-    static QSqlQuery query_delete{db.get_database()};
-    bool res = query_create.exec(QStringLiteral("CREATE TABLE if NOT EXISTS processes_history "
-                                                 "(name TEXT NOT NULL,"
-                                                 " pid INTEGER NOT NULL,"
-                                                 " path TEXT,"
-                                                 " creation_date INTEGER NOT NULL,"
-                                                 " termination_date INTEGER,"
-                                                 " description TEXT,"
-                                                 " user TEXT,"
-                                                 " domain TEXT,"
-                                                 " cmd TEXT )"));
-    if(!res)
-    {
-        qFatal()<<"Couldn't create the table. Error: "<<query_create.lastError().text();
-        return false;
-    }
-    res = query_create.prepare(QStringLiteral("INSERT INTO "
-        "processes_history(name, pid, path, creation_date, termination_date, description, user, domain, cmd) "
-        "VALUES(:name, :pid, :path, :creation_date, :termination_date, :description, :user, :domain, :cmd)"));
-    if(!res)
-    {
-        qFatal()<<"Couldn't prepare the query for process creation. Error: "<<query_create.lastError().text();
-        return false;
-    }
-
-    res = query_delete.prepare(QStringLiteral("UPDATE processes_history SET termination_date = :termination_date "
-                                        "WHERE creation_date = :creation_date AND pid = :pid"));
-    if(!res)
-    {
-        qFatal()<<"Couldn't prepare the query for process deletion. Error: "<<query_delete.lastError().text();
-        return false;
-    }
-    auto start = std::chrono::steady_clock::now();
-    tracker->apply_to_every_elem([](const ProcessInfo& proc){
-        query_create.bindValue(0,proc.name);
-        query_create.bindValue(1,proc.pid);
-        query_create.bindValue(2,proc.file_path);
-        query_create.bindValue(3,proc.creation_date.toSecsSinceEpoch());
-        query_create.bindValue(4,QStringLiteral("NULL"));
-        query_create.bindValue(5,proc.description);
-        query_create.bindValue(6,proc.owner_user);
-        query_create.bindValue(7,proc.owner_domain);
-        query_create.bindValue(8,proc.command_line);
-        if(!query_create.exec())
-            qWarning()<<"couldn't insert new process. Error: "<<query_create.lastError().text();
-    });
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    qDebug()<<elapsed_seconds.count();
-    QObject::connect(tracker,&ProcessTracker::process_created,[](const ProcessInfo& proc){
-        query_create.bindValue(0,proc.name);
-        query_create.bindValue(1,proc.pid);
-        query_create.bindValue(2,proc.file_path);
-        query_create.bindValue(3,proc.creation_date.toSecsSinceEpoch());
-        query_create.bindValue(4,QStringLiteral("NULL"));
-        query_create.bindValue(5,proc.description);
-        query_create.bindValue(6,proc.owner_user);
-        query_create.bindValue(7,proc.owner_domain);
-        query_create.bindValue(8,proc.command_line);
-        if(!query_create.exec())
-            qWarning()<<"couldn't insert new process. Error: "<<query_create.lastError().text();
-    });
-    QObject::connect(tracker,&ProcessTracker::process_deleted,[](const ProcessInfo& proc){
-        query_delete.bindValue(QStringLiteral(":termination_date"),proc.termination_date.toSecsSinceEpoch());
-        query_delete.bindValue(QStringLiteral(":creation_date"),proc.creation_date.toSecsSinceEpoch());
-        query_delete.bindValue(QStringLiteral(":pid"),proc.pid);
-        if(!query_delete.exec())
-            qWarning()<<"couldn't update termination date. Error: "<<query_delete.lastError().text();
-    });
-    return true;
-}
+//Creates db connection with all the necessary db table stuff.
+//Connects process creation/termination signals to store them in a database.
+bool init_connect_db();
 
 int main(int argc, char *argv[])
 {
@@ -103,6 +27,8 @@ int main(int argc, char *argv[])
             break;
         }
     }
+    if(!init_connect_db())
+        app.exit(-1);
     qputenv("QT_QUICK_CONTROLS_CONF",":/DuneCat/imports/qtquickcontrols2.conf");
     qRegisterMetaType<ProcessInfo>("ProcessInfo");
     qmlRegisterType<ProcessTableModel>("TableModels",1,0,"ProcessTableModel");
@@ -126,3 +52,103 @@ int main(int argc, char *argv[])
     return app.exec();
 }
 
+bool init_connect_db()
+{
+    ProcessTracker* tracker = ProcessTracker::get_instance();
+    static DBManager db{"processes_conn","data.db"};
+    db.open();
+
+    static QSqlQuery query_create{db.get_database()};
+    static QSqlQuery query_delete{db.get_database()};
+    bool res = query_create.exec(QStringLiteral("CREATE TABLE if NOT EXISTS processes_history "
+                                                "(name TEXT NOT NULL,"
+                                                " pid INTEGER NOT NULL,"
+                                                " path TEXT,"
+                                                " creation_time INTEGER NOT NULL,"
+                                                " termination_time INTEGER,"
+                                                " description TEXT,"
+                                                " user TEXT,"
+                                                " domain TEXT,"
+                                                " cmd TEXT )"));
+    if(!res)
+    {
+        qFatal()<<"Couldn't create the table. Error: "<<query_create.lastError().text();
+        return false;
+    }
+
+    res = query_create.exec(QStringLiteral("CREATE UNIQUE INDEX if NOT EXISTS unique_pid_time ON processes_history (pid,creation_time)"));
+    if(!res)
+    {
+        qFatal()<<"Couldn't create unique index. Error: "<<query_create.lastError().text();
+        return false;
+    }
+
+    res = query_create.prepare(QStringLiteral("INSERT OR IGNORE INTO "
+                                              "processes_history(name, pid, path, creation_time, termination_time, description, user, domain, cmd) "
+                                              "VALUES(:name, :pid, :path, :creation_time, :termination_time, :description, :user, :domain, :cmd)"));
+    if(!res)
+    {
+        qFatal()<<"Couldn't prepare the query for process creation. Error: "<<query_create.lastError().text();
+        return false;
+    }
+
+    res = query_delete.prepare(QStringLiteral("UPDATE processes_history SET termination_time = :termination_time "
+                                              "WHERE creation_time = :creation_time AND pid = :pid"));
+    if(!res)
+    {
+        qFatal()<<"Couldn't prepare the query for process deletion. Error: "<<query_delete.lastError().text();
+        return false;
+    }
+
+
+    auto start = std::chrono::steady_clock::now();
+    tracker->apply_to_array([](const std::vector<ProcessInfo>& processes){
+        if(db.transaction())
+        {
+            for(const auto& proc : processes)
+            {
+                query_create.bindValue(0,proc.name);
+                query_create.bindValue(1,proc.pid);
+                query_create.bindValue(2,proc.file_path);
+                query_create.bindValue(3,proc.creation_time.toSecsSinceEpoch());
+                query_create.bindValue(4,QStringLiteral("NULL"));
+                query_create.bindValue(5,proc.description);
+                query_create.bindValue(6,proc.owner_user);
+                query_create.bindValue(7,proc.owner_domain);
+                query_create.bindValue(8,proc.command_line);
+                if(!query_create.exec())
+                    qWarning()<<"Couldn't insert new process into history_processes table. Error: "
+                               <<query_create.lastError().text();
+            }
+            if(!db.commit())
+                db.print_last_db_error(QStringLiteral("Couldn't commit changes to history_processes table. Error: "));
+        }
+        else
+            db.print_last_db_error(QStringLiteral("Couldn't start a trancastion to history_processes table. Error: "));
+
+    });
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    qDebug()<<elapsed_seconds.count();
+    QObject::connect(tracker,&ProcessTracker::process_created,[](const ProcessInfo& proc){
+        query_create.bindValue(0,proc.name);
+        query_create.bindValue(1,proc.pid);
+        query_create.bindValue(2,proc.file_path);
+        query_create.bindValue(3,proc.creation_time.toSecsSinceEpoch());
+        query_create.bindValue(4,QStringLiteral("NULL"));
+        query_create.bindValue(5,proc.description);
+        query_create.bindValue(6,proc.owner_user);
+        query_create.bindValue(7,proc.owner_domain);
+        query_create.bindValue(8,proc.command_line);
+        if(!query_create.exec())
+            qWarning()<<"Couldn't insert new process into history_processes table. Error: "<<query_create.lastError().text();
+    });
+    QObject::connect(tracker,&ProcessTracker::process_deleted,[](const ProcessInfo& proc){
+        query_delete.bindValue(QStringLiteral(":termination_time"),proc.termination_time.toSecsSinceEpoch());
+        query_delete.bindValue(QStringLiteral(":creation_time"),proc.creation_time.toSecsSinceEpoch());
+        query_delete.bindValue(QStringLiteral(":pid"),proc.pid);
+        if(!query_delete.exec())
+            qWarning()<<"Couldn't update termination date. Error: "<<query_delete.lastError().text();
+    });
+    return true;
+}
