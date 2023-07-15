54c3812cc74527e35a0fbeb561254cae28c6d967
Nikita Novikov <ioki9@hotmail.com>
2023-07-15 05:48:30 +0300

    Fix/issue 25/fix settings class variables writeread in multithreaded environment (#30)
    * Update cmake.yml
    
    * Feature/issue 17/store history of processes in a database (#21)
    
    * remove [[maybe_unused]]
    
    * add dbmanager class
    
    * add constexpr to a variable
    
    * add simple dbmanager class.
    
    * refactor: replace check with more convenient function
    
    * add default constructor,getters and create_connection function
    
    * Switch some func from QStringView to const QString&
    
    * create and use print error for db
    
    * create table exists function
    
    * create table remove function
    
    * rename class to DCBase
    
    * add DBReader empty class
    
    * refactor(database): rename class. Make func more appropriate.
    
    * refactor(db): Add inline. Add get_database function.
    
    * fix(db): Add copy constructor. Delete move constructor.
    
    It have more sense to for this class to have only the copy constructor,
    since all we do right now is copying the database anyway.
    
    * refactor(db): make copy constructors explicit.
    
    * refactor(db): function usage and small refactor
    
    * fix(db): Multithreaded access to open connections map. Fix int length.
    
    * feature(db): Unit tests for DBManager class functions.
    
    * Update dbmanager.h
    
    Fix(db): change open connections count type to int8_t.
    
    * Update dbmanager.cpp
    
    Fix(db): change open connections count type to int8_t.
    
    * fix(wmi): Bug with gathering process description.
    
    * Refactor(PTrck): Make ProcessTracker singleton.
    
    * Fix(PTrck): Move comparison function to src files.
    
    * Refactor(PTrck): Move to shared_lock mechanism
    
    * Feature(db): Store and update the process history table.
    
    * Refactor(Pinfo): Rename variables, set termination time on Linux/Mac.
    
    * Fix(db): Fix print_last_db_error and make it public.
    
    * Refactor(db): Commit first batch of processes using transaction.
    
    * Feature(PTrck): Create a function for using proc array with callback.
    
    * Feature(PTrck): Add function to set seDebugPrivilege on Windows.
    
    * Fix(PTrck): use 0 if creation date is not valid.
    
    * Fix(PTrck): Getting process creation time function fix.
    
    * Refactor(PTrck): Process tracker notifications refactor.
    
    * Feature(db): Set termination date to last observed by app.
    
    * Fix(PTrck): Remove return from void func.
    
    * Fix(PTrck): Fix preprocessor for Mac
    
    * Fix(PTrck): Fix syntactic error on Mac
    
    * Fix(PTrck): Fix process creation notifications.
    
    * Fix(PTrck): Fix emit of signal on Mac
    
    * Refactor(PTrck): remove preprocessor for Mac
    
    * Fix(PTrck): Add class namespace to function
    
    * Fix(PTrck): Add const qualifier to definition
    
    * Update tst_coretests.cpp
    
    * Update dbmanager.cpp
    
    * make bool atomic
    
    * Update dbmanager.cpp
    
    * Update tst_coretests.cpp
    
    * Update dbmanager.cpp
    
    * Feature/issue 24/implement hide in tray mechanism (#26)
    
    * Feature(Tray): Create basic SystemTray class.
    
    * Refactor(db): Use default name for database in constructor.
    
    * Refactor(db): Use default name for database in constructor.
    
    * Feature(Tray): Tray icon impl. Startup as hidden in tray.
    
    * Fix(db): Remove unnecessary capture in lambda.
    
    * Fix(db): Syntax error correction.
    
    * Refactor(qml): Use lazy initialization on admin panel.
    
    * Fix timestamps recording in process history table (#27)
    
    * Feature(Tray): Create basic SystemTray class.
    
    * Refactor(db): Use default name for database in constructor.
    
    * Refactor(db): Use default name for database in constructor.
    
    * Feature(Tray): Tray icon impl. Startup as hidden in tray.
    
    * Fix(db): Remove unnecessary capture in lambda.
    
    * Fix(db): Syntax error correction.
    
    * Refactor(qml): Use lazy initialization on admin panel.
    
    * Fix(db): Fix process creation TS for windows.
    
    Creation timestamp now uses and boot time timestamp where it can't
    access it through the process. When app has admin rights it is usally
    can't access only system processes that are usually launched on startup.
    
    * Fix(Settings): Make variables atomic.

--- src/DuneCat_App/settings.cpp
+++ src/DuneCat_App/settings.cpp
@@ -14,8 +14,8 @@ Settings::Settings(QObject *parent)
 
 void Settings::read_all_settings()
 {
-    m_window_size = QSize{m_settings.value(QStringLiteral("MainWindow/width"),1024).toInt(),
-                          m_settings.value(QStringLiteral("MainWindow/height"),768).toInt()};
+    m_window_width = m_settings.value(QStringLiteral("MainWindow/width"),1024).toInt();
+    m_window_height = m_settings.value(QStringLiteral("MainWindow/height"),768).toInt();
     m_window_maximized = m_settings.value(QStringLiteral("MainWindow/maximized"),false).toBool();
     m_autostart = m_settings.value(QStringLiteral("AutoStart"),false).toBool();
 }
@@ -30,7 +30,7 @@ bool Settings::set_autostart(bool is_on)
 
 bool Settings::is_autostart_on() const
 {
-    return m_autostart;
+    return m_autostart.load();
 }
 
 qint64 Settings::get_last_alive() const
@@ -40,7 +40,7 @@ qint64 Settings::get_last_alive() const
 
 bool Settings::is_window_maximized() const
 {
-    return m_window_maximized;
+    return m_window_maximized.load();
 }
 
 void Settings::set_window_maximized(bool maximized)
@@ -53,24 +53,24 @@ void Settings::set_window_maximized(bool maximized)
 
 int Settings::get_window_height() const
 {
-    return m_window_size.height();
+    return m_window_height.load();
 }
 
 void Settings::set_window_height(int height)
 {
-    m_window_size.setHeight(height);
+    m_window_height = height;
     m_settings.setValue(QStringLiteral("MainWindow/height"),height);
     emit window_height_changed();
 }
 
 int Settings::get_window_width() const
 {
-    return m_window_size.width();
+    return m_window_width.load();
 }
 
 void Settings::set_window_width(int width)
 {
-    m_window_size.setWidth(width);
+    m_window_width = width;
     m_settings.setValue(QStringLiteral("MainWindow/width"),width);
     emit window_width_changed();
 }
